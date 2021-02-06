306f3cc5bfe15c3dc4252e614edaaa7de32aa109
Nadya <yfdq@mail.ru>
2021-02-06 16:56:44 +0300

    добавлено адаптивное  меню для мобильной весрии

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,10 @@
 (function () {
     let toogle = document.querySelector('.header_nav__toggle');
+    let menuOpen = document.querySelector('.header_toggle__menu');
+    let overlay = document.querySelector('.mobile_overlay');
     toogle.addEventListener('click', () => {
         toogle.classList.toggle('opened');
+        menuOpen.classList.toggle('menu_active');
+        overlay.classList.toggle('overlay_active');
     });
 })();
